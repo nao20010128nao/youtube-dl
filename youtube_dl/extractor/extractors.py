fa7bace343fa5d9db0fa2db7b8764a1fd69ca7ac
nao20010128nao <nao20010128@gmail.com>
2020-12-03 08:28:48 +0000

    Files as of https://qiita.com/nao20010128nao/items/2dea7f263d0fd5ca29b9

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1545,3 +1545,5 @@ from .zattoo import (
 from .zdf import ZDFIE, ZDFChannelIE
 from .zingmp3 import ZingMp3IE
 from .zype import ZypeIE
+
+from .whowatch import WhoWatchIE
---
+++ youtube_dl/extractor/whowatch.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from .common import InfoExtractor
+
+
+class WhoWatchIE(InfoExtractor):
+    _VALID_URL = r'https?://whowatch\.tv/viewer/(?P<id>\d+)/?'
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+
+        api_url = 'https://api.whowatch.tv/lives/' + video_id + '/play?referer=https%3A%2F%2Fwhowatch.tv%2F'
+        # URL、IDの順で指定する
+        live_data = self._download_json(api_url, video_id)
+
+        # HLSのURL
+        hls_url = live_data['hls_url']
+        # とりあえずHLSのフォーマットを検索する
+        formats = self._extract_m3u8_formats(
+            hls_url, video_id, ext='mp4', entry_protocol='m3u8_native',
+            m3u8_id='hls')
+        # 並び替える。これによって何も設定しない状態で最高画質をダウンロードするようにする
+        self._sort_formats(formats)
+
+        return {
+          'id': video_id,
+          # 鉤括弧があったので同時に外してしまう
+          'title': live_data['share_info']['live_title'][1:-1],
+          # フォーマット一覧
+          'formats': formats,
+          # これは生放送です
+          'is_live': True,
+        }
