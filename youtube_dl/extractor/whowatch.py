8959bfa138a8f16f05831c97fab981a4927e6133
nao20010128nao <nao20010128@gmail.com>
2021-01-04 03:54:43 +0000

    Files as of https://qiita.com/nao20010128nao/items/43d82cdd0278ae25f725

--- youtube_dl/extractor/whowatch.py
+++ youtube_dl/extractor/whowatch.py
@@ -11,9 +11,9 @@ class WhoWatchIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        api_url = 'https://api.whowatch.tv/lives/' + video_id + '/play?referer=https%3A%2F%2Fwhowatch.tv%2F'
         # URL、IDの順で指定する
-        live_data = self._download_json(api_url, video_id)
+        live_data = self._download_json('https://api.whowatch.tv/lives/' + video_id + '/play', video_id)
+        metadata = self._download_json('https://api.whowatch.tv/lives/' + video_id, video_id)
 
         # デバッグ用: live_dataを表示する
         # self.to_screen(live_data)
@@ -32,12 +32,22 @@ class WhoWatchIE(InfoExtractor):
         # 並び替える。これによって何も設定しない状態で最高画質をダウンロードするようにする
         self._sort_formats(formats)
 
+        # その他の情報を取り出す
+        uploader = metadata['live']['user']['name']
+        uploader_id = metadata['live']['user']['user_path']
+        thumbnail = metadata['live']['latest_thumbnail_url']
+
         return {
-          'id': video_id,
-          # 鉤括弧があったので同時に外してしまう
-          'title': live_data['share_info']['live_title'][1:-1],
-          # フォーマット一覧
-          'formats': formats,
-          # これは生放送です
-          'is_live': True,
+            'id': video_id,
+            # 鉤括弧があったので同時に外してしまう
+            'title': live_data['share_info']['live_title'][1:-1],
+            # フォーマット一覧
+            'formats': formats,
+            # これは生放送です
+            'is_live': True,
+            # 投稿者情報
+            'uploader': uploader,
+            'uploader_id': uploader_id,
+            # サムネイル
+            'thumbnail': thumbnail,
         }
